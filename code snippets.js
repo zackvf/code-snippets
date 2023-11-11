9bdbf04d950847c697ca06a6d3007e870da8be6c
Zack Fons <nofcaz@protonmail.com>
2023-11-11 14:18:21 -0500

    Added snippets

--- code snippets.js
+++ code snippets.js
@@ -19,3 +19,67 @@ function rangeOfNumbers(startNum, endNum) {
     return rangeArr;
   }
 }
+
+
+// basic calculator
+const calculator = {
+    add: function(x, y) {
+        return x + y;
+    },
+    subtract: function(x, y) {
+        return x - y;
+    },
+    multiply: function(x, y) {
+        return x * y;
+    },
+    divide: function(x, y) {
+        return x / y;
+    }
+}
+
+
+// basic clock
+function clock() {
+    setInterval(() => {
+        console.clear();
+        console.log(new Date().toLocaleTimeString());
+    }, 1000);
+}
+
+
+// basic stopwatch
+function Stopwatch() {
+    let startTime;
+    let endTime;
+    let running;
+    let duration = 0;
+    // can also be written as let startTime, endTime, running, duration = 0
+
+    this.start = function () {
+        if (running)
+            throw new Error("Stopwatch already started.");
+        running = true;
+        startTime = new Date();
+    }
+
+    this.stop = function () {
+        if (!running)
+            throw new Error("Timer already stopped.");
+        running = false;
+        endTime = new Date();
+        let seconds = (endTime.getTime() - startTime.getTime()) / 1000;
+        duration += seconds;
+    }
+    
+    this.reset = function() {
+        startTime = null;
+        endTime = null;
+        running = false;
+        duration = 0;
+    }
+
+    // read-only property, being a getter function, of the Stopwatch constructor function so as to not accidentally overwrite the duration outside of the timer
+    Object.defineProperty(this, 'duration', {
+        get: function() { return duration; }
+    });
+}
