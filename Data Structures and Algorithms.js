1ee6b123b5753427f576477058d741c5d5bae458
Zack Fons <nofcaz@protonmail.com>
2023-11-28 11:19:36 -0500

    Added notes

--- Data Structures and Algorithms.js
+++ Data Structures and Algorithms.js
@@ -550,3 +550,15 @@ Animal.prototype = { // this can be utilized as a 'supertype' among various inst
     console.log('test');
   } 
 };
+// direct way to inherit a prototype object's properties/methods to instances of that object - Object.create(obj) creates a new object, and sets obj as the new object's prototype. Recall that the prototype is like the "recipe" for creating an object. By setting the prototype of animal to be the prototype of Animal, you are effectively giving the animal instance the same "recipe" as any other instance of Animal.
+// ex.
+function Animal() { }
+Animal.prototype = {
+  constructor: Animal,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+let duck = Object.create(Animal.prototype);
+let beagle = Object.create(Animal.prototype);
+// if Object.create(Animal) is used instead, the reference to the eat function will not exist
