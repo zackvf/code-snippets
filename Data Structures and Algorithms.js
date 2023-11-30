c99f8e9b7a1bc985709cfc4140930617fad486e3
Zack Fons <nofcaz@protonmail.com>
2023-11-30 11:58:45 -0500

    Added notes

--- Data Structures and Algorithms.js
+++ Data Structures and Algorithms.js
@@ -642,4 +642,30 @@ let funModule = (function() {
 
 // functional programming
 // Functional programming is a style of programming where solutions are simple, isolated functions, without any side effects outside of the function scope, resulting in the following behavior: input for the function -> process the function undertakes -> output the function provides. Functional programming is about the following: Isolated functions - there is no dependence on the state of the program, which includes global variables that are subject to change / Pure functions - the same input always gives the same output / Functions with limited side effects - any changes, or mutations, to the state of the program outside the function are carefully controlled.
+// callback functions - Callbacks are the functions that are passed into another function to decide the invocation of that function. For example, in filter(), the callback function tells JavaScript the criteria for how to filter an array.
+// Functions that can be assigned to a variable, passed into another function, or returned from another function just like any other normal value, are called "first-class" functions. In JavaScript, all functions are first class functions.
+// The functions that take a function as an argument, or return a function as a return value, are called higher order functions.
+// When functions are passed in to or returned from another function, then those functions which were passed in or returned can be called a lambda.
+// ex.
+// Function that returns a string representing a cup of green tea
+const prepareGreenTea = () => 'greenTea';
+// Function that returns a string representing a cup of black tea
+const prepareBlackTea = () => 'blackTea';
+/*
+Given a function (representing the tea type) and number of cups needed, the following function returns an array of strings (each representing a cup of a specific type of tea).
+*/
+const getTea = (prepareTea, numOfCups) => {
+  const teaCups = [];
+  for(let cups = 1; cups <= numOfCups; cups += 1) {
+    const teaCup = prepareTea();
+    teaCups.push(teaCup);
+  }
+  return teaCups;
+};
+const tea4GreenTeamFCC = getTea(prepareGreenTea, 27);
+const tea4BlackTeamFCC = getTea(prepareBlackTea, 13);
+console.log(
+  tea4GreenTeamFCC,
+  tea4BlackTeamFCC
+); // expected output: [ 'greenTea' x27] [ 'blackTea' x13]
 // 
