16e7ad3c328d4e0cd50ddbeb99c44caebde3fdf0
Zack Fons <nofcaz@protonmail.com>
2023-12-25 15:10:51 -0500

    Updated snippets

--- snippets.js
+++ snippets.js
@@ -285,3 +285,20 @@ async function fetchStarship() {
   const data = await response.json();
   console.log(data);
 }
+
+// rewrite the below async function to no longer user .then() from Promise
+const urls = [
+  'https://jsonplaceholder.typicode.com/users',
+  'https://jsonplaceholder.typicode.com/posts',
+  'https://jsonplaceholder.typicode.com/albums'
+]
+
+const getData = async function() {
+  const [ users, posts, albums ] = await Promise.all(urls.map(url =>
+      fetch(url).then(resp => resp.json())
+  ));
+  console.log('users', users);
+  console.log('posta', posts);
+  console.log('albums', albums);
+}
+// result:
