f61e6c1d9cbd77d66f108c8bdcb901ff5cc142c0
Zack Fons <nofcaz@protonmail.com>
2024-03-09 23:46:42 -0500

    Updated notes

--- Data Structures and Algorithms.js
+++ Data Structures and Algorithms.js
@@ -364,7 +364,7 @@ function findLongestWordLength(str) { // create a new function with a single, us
   return longestWord; // after looping through the array of words from the user-passed string, return the number of characters of the word with the most letters/characters
 }
 // return largest number among nested arrays - procedurally
-function largestOfFour(arr) { 
+function largestOfFour(arr) { // create a new function with a single, user-passed array parameter
   const finalList = [];
   for (let i = 0; i < arr.length; i++) {
     let largestNumber = arr[i][0];
