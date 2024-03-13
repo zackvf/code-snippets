e50a55eb3ce5f651f03415d5beb41f26aa6ffec4
Zack Fons <nofcaz@protonmail.com>
2024-03-12 22:37:53 -0400

    Updated notes

--- Data Structures and Algorithms.js
+++ Data Structures and Algorithms.js
@@ -367,7 +367,7 @@ function findLongestWordLength(str) { // create a new function with a single, us
 function largestOfFour(arr) { // create a new function with a single, user-passed array parameter, which is expected to be a nested array of arrays
   const finalList = []; // create a new, empty array variable to hold the largest number from each nested array
   for (let i = 0; i < arr.length; i++) { // loop through the user-passed array of arrays, starting at the first nested array and incrementing by one each loop until the length of the user-passed array has been reached
-    let largestNumber = arr[i][0];
+    let largestNumber = arr[i][0]; // with each loop iteration, set the current largest number to the current nested array's first value
     for (let j = 1; j < arr[i].length; j++) {
       if (arr[i][j] > largestNumber) {
         largestNumber = arr[i][j];
