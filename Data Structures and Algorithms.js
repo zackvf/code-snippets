a299bde46313157871db5b76fb6e32caf12f6b1d
Zack Fons <nofcaz@protonmail.com>
2024-05-09 23:38:32 -0400

    Updated notes

--- Data Structures and Algorithms.js
+++ Data Structures and Algorithms.js
@@ -427,7 +427,7 @@ function isBool(value) { // create a new function that takes one argument, being
 }
 // capitalize the first letter in each word in a string to achieve 'title case'
 function titleCase(str) { // create a new function that takes one argument, being a string of any length of characters
-  const strArr = str.split(" "); // 
+  const strArr = str.split(" "); // set up a new variable that contains the array of the words within the string
   let newTitle = [];
   for (let letter in strArr) {
     newTitle[letter] = strArr[letter][0].toUpperCase() + strArr[letter].slice(1).toLowerCase();
