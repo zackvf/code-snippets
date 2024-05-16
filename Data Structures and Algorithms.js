756123d055a93786b97eabe355135ef3abf9921a
Zack Fons <zack@Zacks-MacBook-Pro.local>
2024-05-15 22:55:49 -0400

    Updated notes

--- Data Structures and Algorithms.js
+++ Data Structures and Algorithms.js
@@ -428,7 +428,7 @@ function isBool(value) { // create a new function that takes one argument, being
 // capitalize the first letter in each word in a string to achieve 'title case'
 function titleCase(str) { // create a new function that takes one argument, being a string of any length of characters
   const strArr = str.split(" "); // set up a new variable that contains the array of the words within the string
-  let newTitle = []; // create a new variable to contain an empty array by default, which is to be used to contain the updated version of the string 
+  let newTitle = []; // create a new variable to contain an empty array by default, which is to be used to contain the updated string
   for (let letter in strArr) {
     newTitle[letter] = strArr[letter][0].toUpperCase() + strArr[letter].slice(1).toLowerCase();
   }
